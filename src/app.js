bc1a76b3f36864faf7648ee7749d71f217365de5
Vipin Kumar <vipul15kumar@gmail.com>
2025-01-05 19:04:00 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,35 +1,19 @@
 const express = require("express");
-
+const { adminAuth, userAuth } = require("./middlewares/auth");
+// const db = require("./config/database");
 const app = express();
 
-// app.use("/route", rH, [rH2, rH3], rH4, rH5)
+app.get("/user", userAuth, (req, res) => {
+  res.send("User data sent");
+});
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Handling the route user 1 !!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 2 !!");
-    // res.send("2nd Response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 3 !!");
-    // res.send("3rd Response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 4 !!");
-    // res.send("4th Response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 5 !!");
-    res.send("5th Response");
-  }
-);
+app.get("/admin/getAllData", adminAuth, (req, res) => {
+  res.send("All data sent");
+});
+
+app.get("/admin/deleteUser", adminAuth, (req, res) => {
+  res.send("Deleted a user");
+});
 
 app.listen(5000, () => {
   console.log("Server is listening at PORT: 5000");
